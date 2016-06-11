457e41138f623abee81aa0b8f5dc2498cb81f3b3
Yashodhan Kocharekar <ykochare@tibco.com>
2016-06-11 20:55:30 +0530

    first working version using trie data structure , also implements Bonous points

--- src/main/java/com/github/yashk/contacts/Main.java
+++ src/main/java/com/github/yashk/contacts/Main.java
@@ -1,7 +1,10 @@
 package com.github.yashk.contacts;
 
+import org.apache.commons.collections4.trie.PatriciaTrie;
+
 import java.io.Console;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -12,9 +15,13 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String args[]){
-        
+
         Console console = System.console();
         List<String> contacts = new ArrayList<String>();
+        final PatriciaTrie<String> trie = new PatriciaTrie<String>();
+
+
+
         if(console!=null){
             String line;
             while((line=console.readLine("%s","1) Add contact 2) Search 3) Exit\n"))!=null) {
@@ -22,14 +29,25 @@ public class Main {
                 try {
                     switch (Integer.parseInt(line)) {
                         case 1:
-                            contacts.add(console.readLine("%s", "Enter name:"));
+                            String name = console.readLine("%s", "Enter name:");
+                            contacts.add(name);
+                            List<String> list = Arrays.asList(name.split(" "));
+                            System.out.println(list);
+                            if(list.size() > 1){
+                                trie.put(list.get(0)+list.get(1),name);
+                                trie.put(list.get(1)+list.get(0),name);
+
+                            }else{
+                                trie.put(list.get(0),name);
+
+                            }
                             break;
                         case 2:
                             final String searchTerm = console.readLine("%s", "Enter name:");
-                            contacts.stream().filter(s -> s.contains(searchTerm)).forEach(System.out::println);
+                            trie.prefixMap(searchTerm).forEach((k,v) -> System.out.println(v));
                             break;
                         case 3:
-                            console.printf("%s", "Happy Searching");
+                            console.printf("%s", "Happy Searching\n");
                             return;
                     }
 
